5c9336c9b7083b44622f6aebd0896cc0730f1010
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-05-06 09:25:24 -0400

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -73,10 +73,55 @@ for (let turtle of ninjaTurtles){
     console.log(turtle.toUpperCase());
 };
 
-
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Pottter', 'Imagination of Life', 'Snatch', 'Fast and the Furious'];
+
+console.log(favMovies.indexOf('Titanic'));
+
+// 1. sort method actually keeps the sorted version of the array which is not what I was expecting, it seems permanently altered
+console.log(favMovies.sort());
+console.log(favMovies);
+
+// 2. pop
+console.log(favMovies.pop());
+
+// 3. push
+favMovies.push("Guardians of the Galaxy");
+console.log(favMovies);
+
+// 4. reverse
+console.log(favMovies.reverse());
+
+// 5. shift
+favMovies.shift();
+console.log(favMovies);
+
+// 6. unshift returns the new length of the array
+favMovies.unshift("Batman and Robin");
+console.log(favMovies);
+
+// 7. splice did permanently alter our array
+console.log(favMovies.indexOf('Django Unchained'));
+favMovies.splice(15, 1, "Avatar");
+console.log(favMovies);
+
+// 8. slice does not permanently change the original array
+secondFavMovies = favMovies.slice(9);
+console.log(favMovies);
+console.log(secondFavMovies);
+
+// slice does not permanently alter the original array, it only returns the new portion of the array that was sliced from the original
+
+
+
+
+
+
+
+
+
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
